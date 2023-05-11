a1d46a4718259f38930727806c7be9fa73d33263
Nicolae Marius Ghergu <nicolae.ghergu@s.unibuc.ro>
2023-05-11 20:39:05 +0300

    upload files

---
+++ location-spoof-macos/location_spoof_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  location_spoof_macosApp.swift
+//  location-spoof-macos
+//
+//  Created by Nicolae Marius Ghergu on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct location_spoof_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ location-spoof-macosUITests/location_spoof_macosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  location_spoof_macosUITestsLaunchTests.swift
+//  location-spoof-macosUITests
+//
+//  Created by Nicolae Marius Ghergu on 11.05.2023.
+//
+
+import XCTest
+
+final class location_spoof_macosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
